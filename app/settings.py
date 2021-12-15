a38e4b40d83aa13400d5d7428805a9d1aaea1af5
KentoKawasaki <projectdivakkmd@gmail.com>
2021-12-15 14:59:20 +0900

    changed settings.py

--- app/settings.py
+++ app/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls' #プロジェクト名をxxxに入れる
